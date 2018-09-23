b3c210fc5996c84ba67b7814a548914469b59fba
cxyang <yangchenxi1220@gmail.com>
2018-09-23 22:48:28 +0800

    userLogin
    Add user login

--- mobile/src/main/java/com/example/cxyang/pomelotiming/MainActivity.java
+++ mobile/src/main/java/com/example/cxyang/pomelotiming/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.cxyang.pomelotiming;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String SERVER_URL = "http://10.222.254.12";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +31,36 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //RequestQueue queue = Volley.newRequestQueue(this);
+        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+
+        JSONObject jsonObject = new JSONObject();
+        try{
+            jsonObject.put("username", message);
+        }catch (JSONException e1){
+            e1.printStackTrace();
+        }
+        Log.e("post",jsonObject.toString());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, SERVER_URL, jsonObject,
+                new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("normalResponse", response.toString());
+            }
+            }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError arg0){
+                Log.e("errorResponse", arg0.toString());
+            }
+        });
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
