471970016aa1dcf46e4e3d598d50777abbc251a5
cxyang <yangchenxi1220@gmail.com>
2018-09-24 17:05:56 +0800

    login3.0
    change configuration & sending login info

--- mobile/src/main/java/com/example/cxyang/pomelotiming/DisplayMessageActivity.java
+++ mobile/src/main/java/com/example/cxyang/pomelotiming/DisplayMessageActivity.java
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
--- mobile/src/main/java/com/example/cxyang/pomelotiming/MainActivity.java
+++ mobile/src/main/java/com/example/cxyang/pomelotiming/MainActivity.java
@@ -7,10 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -18,10 +23,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
+    String localHost = "http://10.222.254.12:80";
+    String serverHost = "http://45.32.5.192:80";
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    public static final String SERVER_URL = "http://10.222.254.12";
+    public static final String SERVER_URL = "http://10.222.254.12:80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void onLogin(View view){
         //RequestQueue queue = Volley.newRequestQueue(this);
-        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).
-                getRequestQueue();
+        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
 
         EditText editText = (EditText) findViewById(R.id.editText);
         String username = editText.getText().toString();
@@ -41,21 +49,19 @@ public class MainActivity extends AppCompatActivity {
         EditText editText2 = (EditText) findViewById(R.id.editText2);
         String passwd = editText2.getText().toString();
 
-        JSONObject jsonObject = new JSONObject();
-        try{
-            jsonObject.put("username", username);
-            jsonObject.put("passwd", passwd);
-        }catch (JSONException e1){
-            e1.printStackTrace();
-        }
-        Log.e("post",jsonObject.toString());
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("username", username);
+        params.put("passwd", passwd);
+
+        Log.e("post", params.toString());
 
         String loginString = SERVER_URL + "/login";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, loginString, jsonObject,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, loginString, new JSONObject(params),
                 new Response.Listener<JSONObject>(){
             @Override
             public void onResponse(JSONObject response) {
                 Log.e("normalResponse", response.toString());
+                System.out.print(response.toString());
             }
             }, new Response.ErrorListener(){
             @Override
@@ -64,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        //queue.add(jsonObjectRequest);
 
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, username);
