35bdfa152a4a4e96739dafeddd12df693a7fc304
caoye <16307130173@fudan.edu.cn>
2018-10-15 22:11:21 +0800

    Syn Project

--- mobile/src/main/java/com/example/cxyang/pomelotiming/FragmentActivity.java
+++ mobile/src/main/java/com/example/cxyang/pomelotiming/FragmentActivity.java
@@ -34,70 +34,51 @@ public class FragmentActivity extends AppCompatActivity {
         fragment2 = new Analysis();
         fragment3 = new Profile();
         fragments = new Fragment[]{fragment1,fragment2,fragment3};
-        lastfragment=0;
+        lastfragment = 0;
         getSupportFragmentManager().beginTransaction().replace(R.id.mainview,fragment1).show(fragment1).commit();
-        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bnv);
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bnv);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(changeFragment);
     }
     //判断选择的菜单
-    private BottomNavigationView.OnNavigationItemSelectedListener changeFragment= new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener changeFragment = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            switch (item.getItemId())
-            {
-                case R.id.id1:
-                {
-                    if(lastfragment!=0)
-                    {
+            switch (item.getItemId()) {
+                case R.id.id1: {
+                    if (lastfragment != 0) {
                         switchFragment(lastfragment,0);
-                        lastfragment=0;
-
+                        lastfragment = 0;
                     }
-
                     return true;
                 }
-                case R.id.id2:
-                {
-                    if(lastfragment!=1)
-                    {
+                case R.id.id2: {
+                    if (lastfragment != 1) {
                         switchFragment(lastfragment,1);
-                        lastfragment=1;
-
+                        lastfragment = 1;
                     }
-
                     return true;
                 }
-                case R.id.id3:
-                {
-                    if(lastfragment!=2)
-                    {
+                case R.id.id3: {
+                    if(lastfragment != 2) {
                         switchFragment(lastfragment,2);
-                        lastfragment=2;
-
+                        lastfragment = 2;
                     }
-
                     return true;
                 }
-
-
             }
-
-
             return false;
         }
     };
     //切换Fragment
     private void switchFragment(int lastfragment,int index)
     {
-        FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.hide(fragments[lastfragment]);//隐藏上个Fragment
-        if (fragments[index].isAdded()==false)
+        if (fragments[index].isAdded() == false)
             transaction.add(R.id.mainview,fragments[index]);
 
         transaction.show(fragments[index]).commitAllowingStateLoss();
-
-
     }
 }
